7dde5f250d7464ae205fbafa52812196cf93d58e
Johan Martinsson <jm1974@hotmail.com>
2012-03-03 00:49:43 +0100

    moved construction of items out of constructor

--- src/main/java/fr/xebia/katas/gildedrose/Inn.java
+++ src/main/java/fr/xebia/katas/gildedrose/Inn.java
@@ -6,14 +6,8 @@ import java.util.List;
 public class Inn {
    private List<Item> items;
 
-   public Inn() {
-      items = new ArrayList<Item>();
-      items.add(new Item("+5 Dexterity Vest", 10, 20));
-      items.add(new Item("Aged Brie", 2, 0));
-      items.add(new Item("Elixir of the Mongoose", 5, 7));
-      items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-      items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-      items.add(new Item("Conjured Mana Cake", 3, 6));
+   public Inn(List<Item> initialItems) {
+       items = initialItems;
    }
 
    public void updateQuality() {
@@ -66,12 +60,18 @@ public class Inn {
             }
          }
       }
-
 }
 
    public static void main(String[] args) {
       System.out.println("OMGHAI!");
-      new Inn().updateQuality();
+      List<Item> items = new ArrayList<Item>();
+      items.add(new Item("+5 Dexterity Vest", 10, 20));
+      items.add(new Item("Aged Brie", 2, 0));
+      items.add(new Item("Elixir of the Mongoose", 5, 7));
+      items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+      items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+      items.add(new Item("Conjured Mana Cake", 3, 6));
+      new Inn(items).updateQuality();
    }
 
 }
