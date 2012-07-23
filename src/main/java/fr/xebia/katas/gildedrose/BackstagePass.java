b85b5197605add5b606c5f3a2c991654bdce2db4
Johan Martinsson <martinsson.johan@gmail.com>
2012-07-23 10:56:58 +0200

    started implementing strategy for backstagepass

--- src/main/java/fr/xebia/katas/gildedrose/BackstagePass.java
+++ src/main/java/fr/xebia/katas/gildedrose/BackstagePass.java
@@ -8,20 +8,54 @@ class BackstagePass extends Item {
    
    @Override
    public void update() {
-      if (getSellIn() < 1) {
-         setQuality(0);
-      } else if (getSellIn() < 6) {
-            increaseQuality(3);
-      } else if (getSellIn() < 11) {
-            increaseQuality(2);
-      } else if (getSellIn() < 16) {
-            increaseQuality(1);
-      }
       sellIn--;
+      QualityUpdater qualityUpdater = null;
+      if (getSellIn() < 0) {
+         qualityUpdater = new QualitySetter(0);
+      } else if (getSellIn() < 5) {
+         qualityUpdater = new QualityIncreaser(3);
+      } else if (getSellIn() < 10) {
+         qualityUpdater = new QualityIncreaser(2);
+      } else if (getSellIn() < 15) {
+         qualityUpdater = new QualityIncreaser(1);
+      } else {
+         qualityUpdater = new QualityIncreaser(0);
+      }
+      qualityUpdater.update(this);
    }
     
    public static Item aBackstagePass(int sellIn, int quality) {
       return new BackstagePass(sellIn, quality);
    }
 
+   interface QualityUpdater {
+      void update(Item item);
+   }
+   static class QualitySetter implements QualityUpdater {
+
+      private final int destinationQuality;
+
+      public QualitySetter(int destinationQuality) {
+         this.destinationQuality = destinationQuality;
+      }
+
+      @Override
+      public void update(Item item) {
+         item.setQuality(destinationQuality);
+      }
+   }
+   static class QualityIncreaser implements QualityUpdater {
+
+      private final int amount;
+
+      public QualityIncreaser(int amount) {
+         this.amount = amount;
+      }
+
+      @Override
+      public void update(Item item) {
+         item.increaseQuality(amount);
+      }
+      
+   }
 }
